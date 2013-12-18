b86fc97fb75a93aaa0fde3c6b314e3c6644c5ed5
Yves Goeleven <yves@goeleven.com>
2013-12-18 10:17:35 +0100

    Overriding max retries

---
+++ src/NServiceBus.Azure.Transports.WindowsAzureServiceBus/Config/OverrideMaxRetries.cs
@@ -0,0 +1,31 @@
+﻿namespace NServiceBus.Features
+{
+    using Config;
+    using Config.ConfigurationSource;
+
+    public class OverrideMaxRetries : IProvideConfiguration<TransportConfig>
+    {
+        readonly IConfigurationSource source;
+
+        public OverrideMaxRetries(IConfigurationSource source)
+        {
+            this.source = source;
+        }
+
+        public TransportConfig GetConfiguration()
+        {
+            var c = source.GetConfiguration<AzureServiceBusQueueConfig>();
+            var t = source.GetConfiguration<TransportConfig>();
+            if (c != null && t != null && t.MaxRetries > c.MaxDeliveryCount - 2)
+            {
+                t = new TransportConfig()
+                {
+                    MaximumConcurrencyLevel = t.MaximumConcurrencyLevel,
+                    MaxRetries = c.MaxDeliveryCount - 2,
+                    MaximumMessageThroughputPerSecond = t.MaximumMessageThroughputPerSecond
+                };
+            }
+            return t;
+        }
+    }
+}
