15c7c27ee376cbee44adf1c88ff9524568074354
Yves Goeleven <yves@goeleven.com>
2014-03-03 09:36:57 +0100

    added null ref guard in case someone instantiated this class manually

--- src/NServiceBus.Azure.Transports.WindowsAzureServiceBus/Creation/TopicAutoCreation.cs
+++ src/NServiceBus.Azure.Transports.WindowsAzureServiceBus/Creation/TopicAutoCreation.cs
@@ -18,6 +18,13 @@
             if (!QueueAutoCreation.ShouldAutoCreate)
                 return;
 
+            // to stay backward compat, this used to be autocreated the constructor but is now injected
+            // so if this class was manually instantiated, it could lead to a null ref otherwise.
+            if (TopicCreator == null)
+            {
+                TopicCreator = new AzureServicebusTopicCreator(); 
+            }
+
             var selectedTransport = SettingsHolder.GetOrDefault<TransportDefinition>("NServiceBus.Transport.SelectedTransport");
             if (selectedTransport is AzureServiceBus)
             {
