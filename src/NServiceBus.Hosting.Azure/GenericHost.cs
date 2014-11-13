0882c4fcb33a786e08c408357562f7d4d1d9c0bc
Yves Goeleven <yves@goeleven.com>
2014-11-13 10:25:49 +0100

    Setting host information with new api

--- src/NServiceBus.Hosting.Azure/GenericHost.cs
+++ src/NServiceBus.Hosting.Azure/GenericHost.cs
@@ -4,6 +4,8 @@ namespace NServiceBus.Hosting.Azure
     using System.Collections.Generic;
     using System.Linq;
     using System.Reflection;
+    using System.Security.Cryptography;
+    using System.Text;
     using Config;
     using Config.ConfigurationSource;
     using Helpers;
@@ -119,6 +121,14 @@ namespace NServiceBus.Hosting.Azure
                 {
                     configuration.AzureConfigurationSource();
                 }
+
+                var host = SafeRoleEnvironment.CurrentRoleName;
+                var instance = SafeRoleEnvironment.CurrentRoleInstanceId;
+                var hostId = DeterministicGuid(instance, host);
+                configuration
+                    .UniquelyIdentifyRunningInstance()
+                    .UsingCustomIdentifier(hostId)
+                    .UsingNames(instance, host);
             }
 
             if (moreConfiguration != null)
@@ -144,6 +154,18 @@ namespace NServiceBus.Hosting.Azure
             }
 
             return list.ToArray();
+        }
+
+        static Guid DeterministicGuid(params object[] data)
+        {
+            // use MD5 hash to get a 16-byte hash of the string
+            using (var provider = new MD5CryptoServiceProvider())
+            {
+                var inputBytes = Encoding.Default.GetBytes(String.Concat(data));
+                var hashBytes = provider.ComputeHash(inputBytes);
+                // generate a guid from the hash:
+                return new Guid(hashBytes);
+            }
         }
 
         List<Assembly> assembliesToScan;
