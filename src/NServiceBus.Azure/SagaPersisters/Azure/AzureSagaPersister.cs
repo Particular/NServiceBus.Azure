3185a5acb3a911198aabce83b32b4777bc4e7436
Scooletz <scooletz@gmail.com>
2016-02-25 15:34:20 +0100

    Secondary indices introduced

--- src/NServiceBus.Azure/SagaPersisters/Azure/AzureSagaPersister.cs
+++ src/NServiceBus.Azure/SagaPersisters/Azure/AzureSagaPersister.cs
@@ -3,12 +3,12 @@
     using System;
     using System.Collections.Concurrent;
     using System.Collections.Generic;
-    using System.Net;
     using System.Reflection;
     using System.Runtime.CompilerServices;
     using Microsoft.WindowsAzure.Storage;
     using Microsoft.WindowsAzure.Storage.Table;
     using NServiceBus.Azure;
+    using NServiceBus.SagaPersisters.Azure.SecondaryIndeces;
     using Saga;
 
     /// <summary>
@@ -18,9 +18,11 @@
     {
         readonly bool autoUpdateSchema;
         readonly CloudTableClient client;
+        readonly SecondaryIndexPersister secondaryIndeces;
+
         static readonly ConcurrentDictionary<string, bool> tableCreated = new ConcurrentDictionary<string, bool>();
         static readonly ConditionalWeakTable<object, string> etags = new ConditionalWeakTable<object, string>();
-        
+
         /// <summary>
         /// 
         /// </summary>
@@ -30,6 +32,7 @@
         {
             this.autoUpdateSchema = autoUpdateSchema;
             client = account.CreateCloudTableClient();
+            secondaryIndeces = new SecondaryIndexPersister(GetTable, ScanForSaga, Persist);
         }
 
         /// <summary>
@@ -39,6 +42,7 @@
         /// <param name="saga">the saga entity that will be saved.</param>
         public void Save(IContainSagaData saga)
         {
+            secondaryIndeces.Insert(saga);
             Persist(saga);
         }
 
@@ -73,7 +77,6 @@
             return entity;
         }
 
-
         DictionaryTableEntity GetDictionaryTableEntity(string sagaId, Type entityType)
         {
             var tableName = entityType.Name;
@@ -87,38 +90,13 @@
 
         T ISagaPersister.Get<T>(string property, object value)
         {
-            var type = typeof(T);
-            var tableEntity = GetDictionaryTableEntity(type, property, value);
-            var entity = (T) ToEntity(type, tableEntity);
-
-            if (!Equals(entity, default(T)))
+            var sagaId = secondaryIndeces.FindPossiblyCreatingIndexEntry<T>(property, value);
+            if (sagaId != null)
             {
-                etags.Add(entity, tableEntity.ETag);
+                return Get<T>(sagaId.Value);
             }
 
-            try
-            {
-                return entity;
-            }
-            catch (WebException ex)
-            {
-                // can occur when table has not yet been created, but already looking for absence of instance
-                if (ex.Status == WebExceptionStatus.ProtocolError && ex.Response != null)
-                {
-                    var response = (HttpWebResponse) ex.Response;
-                    if (response.StatusCode == HttpStatusCode.NotFound)
-                    {
-                        return default(T);
-                    }
-                }
-
-                throw;
-            }
-            catch (StorageException)
-            {
-                // can occur when table has not yet been created, but already looking for absence of instance
-                return default(T);
-            }
+            return default(T);
         }
 
         DictionaryTableEntity GetDictionaryTableEntity(Type type, string property, object value)
@@ -132,35 +110,35 @@
 
             if (propertyInfo.PropertyType == typeof(byte[]))
             {
-                query = new TableQuery<DictionaryTableEntity>().Where(TableQuery.GenerateFilterConditionForBinary(property, QueryComparisons.Equal, (byte[]) value));
+                query = new TableQuery<DictionaryTableEntity>().Where(TableQuery.GenerateFilterConditionForBinary(property, QueryComparisons.Equal, (byte[])value));
             }
             else if (propertyInfo.PropertyType == typeof(bool))
             {
-                query = new TableQuery<DictionaryTableEntity>().Where(TableQuery.GenerateFilterConditionForBool(property, QueryComparisons.Equal, (bool) value));
+                query = new TableQuery<DictionaryTableEntity>().Where(TableQuery.GenerateFilterConditionForBool(property, QueryComparisons.Equal, (bool)value));
             }
             else if (propertyInfo.PropertyType == typeof(DateTime))
             {
-                query = new TableQuery<DictionaryTableEntity>().Where(TableQuery.GenerateFilterConditionForDate(property, QueryComparisons.Equal, (DateTime) value));
+                query = new TableQuery<DictionaryTableEntity>().Where(TableQuery.GenerateFilterConditionForDate(property, QueryComparisons.Equal, (DateTime)value));
             }
             else if (propertyInfo.PropertyType == typeof(Guid))
             {
-                query = new TableQuery<DictionaryTableEntity>().Where(TableQuery.GenerateFilterConditionForGuid(property, QueryComparisons.Equal, (Guid) value));
+                query = new TableQuery<DictionaryTableEntity>().Where(TableQuery.GenerateFilterConditionForGuid(property, QueryComparisons.Equal, (Guid)value));
             }
             else if (propertyInfo.PropertyType == typeof(Int32))
             {
-                query = new TableQuery<DictionaryTableEntity>().Where(TableQuery.GenerateFilterConditionForInt(property, QueryComparisons.Equal, (int) value));
+                query = new TableQuery<DictionaryTableEntity>().Where(TableQuery.GenerateFilterConditionForInt(property, QueryComparisons.Equal, (int)value));
             }
             else if (propertyInfo.PropertyType == typeof(Int64))
             {
-                query = new TableQuery<DictionaryTableEntity>().Where(TableQuery.GenerateFilterConditionForLong(property, QueryComparisons.Equal, (long) value));
+                query = new TableQuery<DictionaryTableEntity>().Where(TableQuery.GenerateFilterConditionForLong(property, QueryComparisons.Equal, (long)value));
             }
             else if (propertyInfo.PropertyType == typeof(Double))
             {
-                query = new TableQuery<DictionaryTableEntity>().Where(TableQuery.GenerateFilterConditionForDouble(property, QueryComparisons.Equal, (double) value));
+                query = new TableQuery<DictionaryTableEntity>().Where(TableQuery.GenerateFilterConditionForDouble(property, QueryComparisons.Equal, (double)value));
             }
             else if (propertyInfo.PropertyType == typeof(string))
             {
-                query = new TableQuery<DictionaryTableEntity>().Where(TableQuery.GenerateFilterCondition(property, QueryComparisons.Equal, (string) value));
+                query = new TableQuery<DictionaryTableEntity>().Where(TableQuery.GenerateFilterCondition(property, QueryComparisons.Equal, (string)value));
             }
             else
             {
@@ -196,21 +174,38 @@
         void Persist(IContainSagaData saga)
         {
             var type = saga.GetType();
-            var tableName = type.Name;
+            var table = GetTable(type);
+
+            var partitionKey = saga.Id.ToString();
+
+            var batch = new TableBatchOperation();
+
+            AddObjectToBatch(batch, saga, partitionKey);
+
+            table.ExecuteBatch(batch);
+        }
+
+        private CloudTable GetTable(Type sagaType)
+        {
+            var tableName = sagaType.Name;
             var table = client.GetTableReference(tableName);
             if (autoUpdateSchema && !tableCreated.ContainsKey(tableName))
             {
                 table.CreateIfNotExists();
                 tableCreated[tableName] = true;
             }
+            return table;
+        }
 
-            var partitionKey = saga.Id.ToString();
-                
-            var batch = new TableBatchOperation();
+        private Guid? ScanForSaga(Type sagatype, string propertyName, object propertyValue)
+        {
+            var entity = GetDictionaryTableEntity(sagatype, propertyName, propertyValue);
+            if (entity == null)
+            {
+                return null;
+            }
 
-            AddObjectToBatch(batch, saga, partitionKey);
-
-            table.ExecuteBatch(batch);
+            return Guid.ParseExact(entity.PartitionKey, "D");
         }
 
         void AddObjectToBatch(TableBatchOperation batch, object entity, string partitionKey, string rowkey = "")
@@ -221,39 +216,44 @@
             string etag;
             var update = etags.TryGetValue(entity, out etag);
 
-            var properties = type.GetProperties(BindingFlags.Public | BindingFlags.Instance);
+            var properties = SelectPropertiesToPersist(type);
 
-            var toPersist = ToDictionaryTableEntity(entity, new DictionaryTableEntity { PartitionKey = partitionKey, RowKey = rowkey, ETag = etag}, properties);
+            var toPersist = ToDictionaryTableEntity(entity, new DictionaryTableEntity { PartitionKey = partitionKey, RowKey = rowkey, ETag = etag }, properties);
 
             //no longer using InsertOrReplace as it ignores concurrency checks
             batch.Add(update ? TableOperation.Replace(toPersist) : TableOperation.Insert(toPersist));
+        }
+
+        internal static PropertyInfo[] SelectPropertiesToPersist(Type sagaType)
+        {
+            return sagaType.GetProperties(BindingFlags.Public | BindingFlags.Instance);
         }
 
         DictionaryTableEntity ToDictionaryTableEntity(object entity, DictionaryTableEntity toPersist, IEnumerable<PropertyInfo> properties)
         {
             foreach (var propertyInfo in properties)
             {
-                if (propertyInfo.PropertyType == typeof (byte[]))
+                if (propertyInfo.PropertyType == typeof(byte[]))
                 {
-                    toPersist[propertyInfo.Name]= new EntityProperty((byte[]) propertyInfo.GetValue(entity, null)) ;
+                    toPersist[propertyInfo.Name] = new EntityProperty((byte[])propertyInfo.GetValue(entity, null));
                 }
-                else if (propertyInfo.PropertyType == typeof (bool))
+                else if (propertyInfo.PropertyType == typeof(bool))
                 {
                     toPersist[propertyInfo.Name] = new EntityProperty((bool)propertyInfo.GetValue(entity, null));
                 }
-                else if (propertyInfo.PropertyType == typeof (DateTime))
+                else if (propertyInfo.PropertyType == typeof(DateTime))
                 {
                     toPersist[propertyInfo.Name] = new EntityProperty((DateTime)propertyInfo.GetValue(entity, null));
                 }
-                else if (propertyInfo.PropertyType == typeof (Guid))
+                else if (propertyInfo.PropertyType == typeof(Guid))
                 {
                     toPersist[propertyInfo.Name] = new EntityProperty((Guid)propertyInfo.GetValue(entity, null));
                 }
-                else if (propertyInfo.PropertyType == typeof (Int32))
+                else if (propertyInfo.PropertyType == typeof(Int32))
                 {
                     toPersist[propertyInfo.Name] = new EntityProperty((Int32)propertyInfo.GetValue(entity, null));
                 }
-                else if (propertyInfo.PropertyType == typeof (Int64))
+                else if (propertyInfo.PropertyType == typeof(Int64))
                 {
                     toPersist[propertyInfo.Name] = new EntityProperty((Int64)propertyInfo.GetValue(entity, null));
                 }
@@ -261,7 +261,7 @@
                 {
                     toPersist[propertyInfo.Name] = new EntityProperty((Double)propertyInfo.GetValue(entity, null));
                 }
-                else if (propertyInfo.PropertyType == typeof (string))
+                else if (propertyInfo.PropertyType == typeof(string))
                 {
                     toPersist[propertyInfo.Name] = new EntityProperty((string)propertyInfo.GetValue(entity, null));
                 }
@@ -333,6 +333,6 @@
             return toCreate;
         }
     }
-    
-  
+
+
 }
---
+++ src/NServiceBus.Azure/SagaPersisters/Azure/SecondaryIndeces/PartitionRowKeyTuple.cs
@@ -0,0 +1,52 @@
+﻿namespace NServiceBus.SagaPersisters.Azure.SecondaryIndeces
+{
+    using Microsoft.WindowsAzure.Storage.Table;
+
+    public sealed class PartitionRowKeyTuple
+    {
+        public string PartitionKey { get; }
+        public string RowKey { get; }
+
+        public PartitionRowKeyTuple(string partitionKey, string rowKey)
+        {
+            PartitionKey = partitionKey;
+            RowKey = rowKey;
+        }
+
+        public void Apply(ITableEntity entity)
+        {
+            entity.PartitionKey = PartitionKey;
+            entity.RowKey = RowKey;
+        }
+
+        private bool Equals(PartitionRowKeyTuple other)
+        {
+            return string.Equals(PartitionKey, other.PartitionKey) && string.Equals(RowKey, other.RowKey);
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj))
+            {
+                return false;
+            }
+            if (ReferenceEquals(this, obj))
+            {
+                return true;
+            }
+            if (obj.GetType() != this.GetType())
+            {
+                return false;
+            }
+            return Equals((PartitionRowKeyTuple) obj);
+        }
+
+        public override int GetHashCode()
+        {
+            unchecked
+            {
+                return ((PartitionKey?.GetHashCode() ?? 0)*397) ^ (RowKey?.GetHashCode() ?? 0);
+            }
+        }
+    }
+}
